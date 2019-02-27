2057c200062942c6a289c226f5d812aeebb46d39
Gabriel Rimes <jivagorimes@gmail.com>
2019-02-27 20:05:52 -0300

    Add confirmation override option to some other subcommands

--- index.js
+++ index.js
@@ -31,6 +31,7 @@ program
   .option('-r, --namespace-regex <namespaceRegex>', 'regular expression to filter the namespace')
   .option('--ignore <ignoredNamespace>', 'namespaces to ignore', collect, [])
   .option('-v, --variable <variable>', 'variable with name and value', collect, [])
+  .option('--environment-alias <environmentAlias>', 'environment alias')
   .action(options => run('setEnv', options))
 
 program
@@ -40,6 +41,7 @@ program
   .option('-r, --namespace-regex <namespaceRegex>', 'regular expression to filter the namespace')
   .option('--ignore <ignoredNamespace>', 'namespaces to ignore', collect, [])
   .option('-s, --source-ref [sourceRef]', 'tag/branch/commit hash from Git')
+  .option('--environment-alias <environmentAlias>', 'environment alias')
   .action(options => run('instantiate', options))
 
 program
@@ -57,6 +59,7 @@ program
   .option('-r, --namespace-regex <namespaceRegex>', 'regular expression to filter the namespace')
   .option('-n, --resources-names <resourcesNames>', 'resources names', collect, [])
   .option('--ignore <ignoredNamespace>', 'namespaces to ignore', collect, [])
+  .option('--environment-alias <environmentAlias>', 'environment alias')
   .option('--replicas <replicas>', 'number of replicas')
   .action(options => run('scale', options))
 
@@ -79,6 +82,7 @@ program
   .command('check-resources')
   .option('-r, --namespace-regex <namespaceRegex>', 'regular expression to filter the namespace')
   .option('-n, --resources-names <resourcesNames>', 'resources names', collect, [])
+  .option('--environment-alias <environmentAlias>', 'environment alias')
   .action(options => run('checkResources', options))
 
 program.parse(process.argv)
